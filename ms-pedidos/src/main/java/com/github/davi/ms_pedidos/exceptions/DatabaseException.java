b0ad5f335ccc3bacb932dd9354c3cbecf95c5b67
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 21:58:04 -0300

    add entidades, carga db

---
+++ ms-pedidos/src/main/java/com/github/davi/ms_pedidos/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.github.davi.ms_pedidos.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
