d1bf9bcf905f48bafb20b58afd51d05aa91d4b08
Davi-Queiroz <162523740+Davi-Queiroz@users.noreply.github.com>
2026-04-01 21:59:01 -0300

    Delete src directory

--- src/main/java/com/github/davi/ms_pedidos/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.github.davi.ms_pedidos.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/github/davi/ms_pedidos/repository/ItemDoPedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.davi.ms_pedidos.repository;
-
-import com.github.davi.ms_pedidos.entities.ItemDoPedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemDoPedidoRepository extends JpaRepository<ItemDoPedido,Long> {
-}
